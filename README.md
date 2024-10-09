bc5169f6af86f481e8e1551cc6be88633b979bdc
Whayn <mayeul.falgayrettes@gmail.com>
2024-10-09 09:35:23 +0200

    feat: initial release

---
+++ README.md
@@ -0,0 +1,104 @@
+# Battery Health Tracker
+
+This application tracks battery health and logs the data. Follow these steps to build the project and set up the scheduled task in Task Scheduler.
+
+> [!IMPORTANT]
+> This is a windows only project!
+
+> [!NOTE]
+> This project is a work in progress and is not yet complete. The instructions provided are for building the project and setting up the scheduled task in Task Scheduler.
+
+## Setting Up
+
+Follow these steps to manually download and set up the Battery Health Tracker task in Task Scheduler with the specified settings.
+
+### Download Instructions
+
+1. Download the executable from the [releases page](
+2. Extract the contents of the zip file to a directory of your choice.
+3. Note the path to the executable, e.g., `C:\Users\user\AppData\Local\Battery Life Tracker\battery-health-tracker.exe`.
+4. Note the path to the working directory, e.g., `C:\Users\user\AppData\Local\Battery Life Tracker`.
+5. Proceed to the [Step-by-Step Instructions](#step-by-step-instructions) to set up the scheduled task in Task Scheduler.
+
+### Step-by-Step Instructions
+
+1. Press `Win + R`, type `taskschd.msc`, and press `Enter`.
+
+2. In the right-hand Actions pane, click on `Create Task...`.
+
+3. **General Tab**:
+
+   - **Name**: Enter `Battery Health Tracker`.
+   - **Description**: (Optional) Enter a description for the task.
+   - **Security options**:
+     - Select `Run whether user is logged on or not`.
+     - Check `Run with highest privileges`.
+   - **Configure for**: Select `Windows 10`.
+
+4. **Triggers Tab**:
+
+   - Click `New...` to create a new trigger.
+   - **Begin the task**: Select `On a schedule`.
+   - **Settings**: Select `Daily`.
+   - **Start**: Set the start date and time (e.g., `10:30 AM`).
+   - **Advanced settings**:
+     - Check `Repeat task every` and set it to `30 minutes`.
+     - Set `for a duration of` to `1 hour`.
+     - Ensure `Enabled` is checked.
+   - Click `OK`.
+
+5. **Actions Tab**:
+
+   - Click `New...` to create a new action.
+   - **Action**: Select `Start a program`.
+   - **Program/script**: Enter the path to your executable, e.g., `C:\Users\user\AppData\Local\Battery Life Tracker\battery-health-tracker.exe`.
+   - **Start in (optional)**: Enter the working directory, e.g., `C:\Users\user\AppData\Local\Battery Life Tracker`.
+   - Click `OK`.
+
+6. **Conditions Tab**:
+
+   - Uncheck `Start the task only if the computer is on AC power`.
+   - Ensure other options are set as needed.
+
+7. **Settings Tab**:
+
+   - Check `Allow task to be run on demand`.
+   - Check `Run task as soon as possible after a scheduled start is missed`.
+   - Check `If the task fails, restart every` and set it to `30 minutes`.
+   - Set `Attempt to restart up to` to `3 times`.
+   - Check `Stop the task if it runs longer than` and set it to `1 hour`.
+   - Ensure other options are set as needed.
+
+8. **Finish**:
+   - Click `OK` to create the task.
+   - If prompted, enter your user credentials to save the task.
+
+## Building Instructions
+
+1. **Clone the Repository**:
+
+   - Open a terminal and clone the repository:
+     ```sh
+     git clone https://github.com/yourusername/battery-health-tracker.git
+     cd battery-health-tracker
+     ```
+
+2. **Install Rust**:
+
+   - If you don't have Rust installed, you can install it from [rust-lang.org](https://www.rust-lang.org/):
+     ```sh
+     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+     ```
+
+3. **Build the Project**:
+
+   - Build the project in release mode:
+     ```sh
+     cargo build --release
+     ```
+
+4. **Locate the Executable**:
+   - After building, the executable will be located in the `target/release` directory:
+     ```sh
+     target/release/battery-health-tracker.exe
+     ```
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+use chrono::Local;
+use scraper::{Html, Selector};
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    if !cfg!(target_os = "windows") {
+        println!("This program only works available on Windows");
+        return;
+    }
+
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let new_path: PathBuf = current_dir.join("battery-reports");
+
+    create_battery_report(&new_path);
+
+    let battery_report_path = new_path.join("battery-report.html");
+
+    let file_content =
+        fs::read_to_string(&battery_report_path).expect("Failed to read battery report.");
+
+    // println!("HTML Content:\n{}", file_content);
+
+    let document = Html::parse_document(&file_content);
+
+    let design_capacity_selector = Selector::parse(
+        "body > table:nth-child(5) > tbody:nth-child(3) > tr:nth-child(5) > td:nth-child(2)",
+    )
+    .expect("Invalid selector");
+
+    for (i, element) in document.select(&design_capacity_selector).enumerate() {
+        println!("Element {}: {:?}", i, element.text().collect::<Vec<_>>());
+    }
+
+    let design_capacity_element = document
+        .select(&design_capacity_selector)
+        .nth(0)
+        .expect("Failed to find the first element matching the selector design_capacity_selector");
+
+    let full_charge_capacity_selector = Selector::parse(
+        "body > table:nth-child(5) > tbody:nth-child(3) > tr:nth-child(7) > td:nth-child(2)",
+    )
+    .expect("Invalid selector");
+    let full_charge_capacity_element = document
+        .select(&full_charge_capacity_selector)
+        .nth(0)
+        .expect(
+            "Failed to find the first element matching the selector full_charge_capacity_selector",
+        );
+
+    let design_capacity_text = design_capacity_element.text().collect::<Vec<_>>().join("");
+    let full_charge_capacity_text = full_charge_capacity_element
+        .text()
+        .collect::<Vec<_>>()
+        .join("");
+
+    let design_capacity = design_capacity_text
+        .replace('\u{202f}', "")
+        .trim_end_matches(" mWh\n\n      ")
+        .to_string();
+    let full_charge_capacity = full_charge_capacity_text
+        .replace('\u{202f}', "")
+        .trim_end_matches(" mWh\n\n      ")
+        .to_string();
+
+    println!("Design capacity: {} mWh", design_capacity);
+    println!("Full charge capacity: {} mWh", full_charge_capacity);
+
+    save_to_csv(&design_capacity, &full_charge_capacity);
+}
+
+fn save_to_csv(design_capacity: &str, full_charge_capacity: &str) {
+    let timestamp = Local::now().format("%Y-%m-%d_%H-%M-%S");
+    let file_path = "battery_capacity.csv";
+
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file_path)
+        .expect("Failed to open file");
+
+    if file.metadata().unwrap().len() == 0 {
+        // Write the header if the file is empty
+        writeln!(file, "timestamp,design_capacity,full_charge_capacity")
+            .expect("Failed to write header");
+    }
+
+    writeln!(
+        file,
+        "{},{},{}",
+        timestamp, design_capacity, full_charge_capacity
+    )
+    .expect("Failed to write record");
+}
+
+fn create_battery_report(directory: &PathBuf) {
+    if !(directory.exists()) {
+        fs::create_dir_all(&directory).expect("Failed to create directory");
+    }
+
+    let output = Command::new("cmd")
+        .current_dir(&directory)
+        .args([
+            "/C",
+            "powercfg",
+            "/batteryreport",
+            "/output",
+            "battery-report.html",
+        ])
+        .output()
+        .expect("Failed to execute command");
+
+    if !output.status.success() {
+        eprintln!("Command failed with status: {}", output.status);
+        eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    } else {
+        println!("Command executed successfully.");
+        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    }
+}
